9cc68ed90bb9276b2fb0058a3890bf7b09e842f9
Sergey Melekhin <sergey@melekhin.me>
2016-10-01 22:18:38 +1000

    First DQN implementation

--- board.py
+++ board.py
@@ -1,6 +1,20 @@
+import numpy as np
+
 class Board(object):
-    def __init__(self, data):
-        self.data = data
+    """Tic tac toe board representation class"""
+
+    def __init__(self, data=None, turn=1):
+        """Initializes new tictactoe Board
+        :param data: 9 element list. 0 element is top left corner, 8 is bottom right. Use np.float32(1) for X and np.float32(-1) for O characters and None for empty squares
+        if data is None, empty board (filed with None) is created
+        :param turn: 1 is for X's turn -1 for O's turn"""
+        self.turn = turn
+        if not data:
+            self.data = [None]*9
+        elif len(data) != 9:
+            raise ValueError("Board size should be 9")
+        else:
+            self.data = data
 
     def analyze_state(self):
         """Analyze board data and return
@@ -27,33 +41,38 @@ class Board(object):
 
         return (0, True)
 
+    def get_vec(self, ):
+        "Float vector board representation for ML applications"
+        float_map = {"X": np.float32(1),
+                     "O": np.float32(-1),
+                     None: np.float32(0)}
+        return [float_map[c] for c in self.data]
+
+    def invert(self):
+        "Invert board to use DQN trained to win with X for playng for O"
+        for i in range(0,9):
+            if self.data[i] == "X":
+                self.data[i] = "O"
+            elif self.data[i] == "O":
+                self.data[i] = "X"
+        self.turn = -self.turn
+
+    def make_move(self, action):
+        """Makes move on current board (modifies its state). Returns reward and terminal status, or None if move makes no sense.
+        :param action: int in range(0,9) - board cell to make move on"""
+        if self.data[action] != None:
+            return None
+        turn_map = {1:"X", -1:"O"}
+        self.data[action] = turn_map[self.turn]
+        self.turn = -self.turn
+
+        return self.analyze_state()
+ 
     def __str__(self):
+        "String representation for debugging output"
         s = "|".join([c or " " for c in self.data[0:3]]) + "\n"
         s += "-----\n"
         s += "|".join([c or " " for c in self.data[3:6]]) + "\n"
         s += "-----\n"
         s += "|".join([c or " " for c in self.data[6:9]])
         return s
-
-
-import unittest
-
-class TestBoard(unittest.TestCase):
-
-    test_tab = [[[None,None,None,None,None,None,None,None,None],(0,False)],
-                [["X","X","X",None,None,None,None,None,None],(1,True)],
-                [["X","X","X","X","X","X","X","X","X"],(1,True)],
-                [["X","O","O","X",None,None,"X",None,None],(1,True)],
-                [["O","O","O",None,None,None,None,None,None],(-1,True)]]
-
-    def test_analyze(self):
-        for test in self.test_tab:
-            b = Board(test[0])
-            r = b.analyze_state()
-            print(b)
-            print(r)
-            self.assertEqual(r,test[1])
-
-if __name__ == '__main__':
-    unittest.main()
-
